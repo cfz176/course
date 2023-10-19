e2e1b1e265309272b1e96ee144e43e04051c538c
chenfuzhou <cfz176@outlook.com>
2023-10-19 18:56:55 +0800

    1.将system配置成最简单的SpringBoot项目

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -1,23 +1,21 @@
-package com.course;
+package com.course.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplilcation {
+public class SystemApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(EurekaApplilcation.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(EurekaApplilcation.class);
+        SpringApplication springApplication = new SpringApplication(SystemApplication.class);
         ConfigurableEnvironment env = springApplication.run(args).getEnvironment();
         LOG.info("----启动成功----");
-        LOG.info("Eureka地址：\t http://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("System地址：\t http://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
