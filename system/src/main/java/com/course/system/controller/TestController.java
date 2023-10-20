6c855845fc063ce1d99a6d0c9e4a6a3e900b2a62
chenfuzhou <cfz176@outlook.com>
2023-10-20 21:48:55 +0800

    1.使用database关联数据库 2.新增数据库脚本

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,14 +1,23 @@
 package com.course.system.controller;
 
+import com.course.system.entity.Test;
+import com.course.system.service.TestService;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private TestService testService;
+
     @RequestMapping("/test")
-    public String test() {
-        return "data";
+    public List<Test> test() {
+        return testService.list();
     }
 
 }
---
+++ system/src/main/java/com/course/system/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.course.system.mapper;
+
+import com.course.system.entity.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    List<Test> list();
+}
