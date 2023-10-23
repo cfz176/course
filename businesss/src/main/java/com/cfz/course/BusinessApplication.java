25646a974ffabaaaa22f4cc4256b1e67c080a911
chenfuzhou <cfz176@outlook.com>
2023-10-23 10:49:29 +0800

    1.怎加business模块 2.纠正system logback.xml打印路径

---
+++ businesss/src/main/java/com/cfz/course/BusinessApplication.java
@@ -0,0 +1,28 @@
+package com.cfz.course;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.server.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(BusinessApplication.class);
+        ConfigurableEnvironment env = springApplication.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("Business地址 \t http://127.0.0.1:{}",env.getProperty("server.port"));
+    }
+}
