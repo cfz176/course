c16a1adb13cb8eb236a614de88d5630cc75eabb0
chenfuzhou <cfz176@outlook.com>
2023-10-24 19:02:48 +0800

    1.网关配置lb://business + 服务名 路由转发

--- gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -1,11 +1,17 @@
 package com.course.gateway;
 
+import io.netty.handler.codec.http.cors.CorsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,6 +24,24 @@ public class GatewayApplication {
         ConfigurableEnvironment env = springApplication.run(args).getEnvironment();
         LOG.info("----启动成功----");
         LOG.info("GateWay地址：\t http://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowCredentials(Boolean.TRUE);
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
     }
 
 }
